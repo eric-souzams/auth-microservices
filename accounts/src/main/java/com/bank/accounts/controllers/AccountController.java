bd39555301af3416e228d7997ebb7ce75c5ea942
Eric Magalhães <15831837+eric-souzams@users.noreply.github.com>
2022-07-25 17:10:48 -0300

    Adding Resilience to Account microservice

--- accounts/src/main/java/com/bank/accounts/controllers/AccountController.java
+++ accounts/src/main/java/com/bank/accounts/controllers/AccountController.java
@@ -8,14 +8,22 @@ import com.bank.accounts.services.impl.AccountServiceConfig;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -24,17 +32,15 @@ public class AccountController {
 
     @Autowired
     private AccountService accountService;
-
     @Autowired
     private AccountServiceConfig accountServiceConfig;
-
     @Autowired
     private LoansFeignClient loansFeignClient;
-
     @Autowired
     private CardsFeignClient cardsFeignClient;
 
     @GetMapping("/{id}")
+    @Bulkhead(name = "bulkheadForCustomerDetails", fallbackMethod = "fallbackDetailsForCustomerSupportApp")
     public ResponseEntity<Account> getAccount(@PathVariable("id") Long customerId) {
         Account account = accountService.findAccountByCustomerId(customerId);
 
@@ -42,6 +48,8 @@ public class AccountController {
     }
 
     @GetMapping("/my-account/{id}")
+//    @CircuitBreaker(name = "detailsForCustomerSupportApp", fallbackMethod = "fallbackDetailsForCustomerSupportApp")
+    @Retry(name = "retryForCustomerDetails", fallbackMethod = "fallbackDetailsForCustomerSupportApp")
     public ResponseEntity<AccountDetailsDto> myAccountDetails(@PathVariable("id") Long customerId) {
         Account account = accountService.findAccountByCustomerId(customerId);
         List<Loan> loans = loansFeignClient.getLoanDetails(customerId);
@@ -57,12 +65,30 @@ public class AccountController {
     }
 
     @GetMapping("/properties")
-    public String getProperties() throws JsonProcessingException {
+    @RateLimiter(name = "rateLimiterForCustomerDetails", fallbackMethod = "fallbackDetailsForCustomerSupportApp2")
+    public ResponseEntity<String> getProperties() throws JsonProcessingException {
         ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
         Properties properties = new Properties(accountServiceConfig.getMsg(), accountServiceConfig.getBuildVersion(),
                 accountServiceConfig.getMailDetails(), accountServiceConfig.getActiveBranches());
 
-        return ow.writeValueAsString(properties);
+        return ResponseEntity.ok(ow.writeValueAsString(properties));
     }
 
+    private ResponseEntity<?> fallbackDetailsForCustomerSupportApp(Exception exception) {
+        Map<String, String> response = new HashMap<>();
+        response.put("timestamp", new Date().toString());
+        response.put("status", String.valueOf(HttpStatus.SERVICE_UNAVAILABLE.value()));
+        response.put("message", "There was a problem trying to connect to the client");
+
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(response);
+    }
+
+    private ResponseEntity<?> fallbackDetailsForCustomerSupportApp2(Throwable throwable) {
+        Map<String, String> response = new HashMap<>();
+        response.put("timestamp", new Date().toString());
+        response.put("status", String.valueOf(HttpStatus.SERVICE_UNAVAILABLE.value()));
+        response.put("message", "There was a problem trying to connect to the client");
+
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(response);
+    }
 }
